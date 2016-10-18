387709c71e5116982b76101383cfc72837b0dc87
csyouk <csyouk@hanmail.net>
2016-10-18 19:54:04 +0900

    Java Web Programming

---
+++ TestCase.java
@@ -0,0 +1,10 @@
+package com.abc.ppl;
+
+public class TestCase {
+
+
+  public static void main(String[] args){
+    System.out.println("test");
+  }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by changsuyouk on 10/18/16.
+ */
+public class Main {
+    public static void main(String args[]){
+        System.out.println("hello");
+    }
+}
