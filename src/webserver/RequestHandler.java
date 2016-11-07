b8fa8a8f3b0126326f55c6e9f8e7b8ef1e1d6cc9
csyouk <csyouk@hanmail.net>
2016-11-07 16:46:28 +0900

    요청에 대해 다른 파일을 서빙할 수 있도록 다시 짜는 중.

--- src/webserver/RequestHandler.java
+++ src/webserver/RequestHandler.java
@@ -5,14 +5,20 @@ import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 /**
  * Created by changsuyouk on 10/18/16.
  */
 public class RequestHandler extends Thread {
     private static int PATH = 1;
-    private static int BEHAVIOR = 1;
+    private static int BEHAVIOR = 2;
+    private static int COMMAND = 0;
+    private static int QUERY = 1;
+    private static int KEY = 0;
+    private static int VALUE = 1;
     private Socket connection;
+    private byte[] body;
 
     public RequestHandler(Socket connectionSocket){
         this.connection = connectionSocket;
@@ -25,38 +31,101 @@ public class RequestHandler extends Thread {
 
             BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
             String line = br.readLine();
-            System.out.println("line : " + line);
-            String[] tokens = line.split(" ");
-            byte[] body = "".getBytes();
-            if(tokens[PATH].equals("/index.html")){
-                String prefix = "/Users/changsuyouk/java/src/webserver/webapp";
-                body = readFile(prefix + tokens[PATH]);
-            } else {
-                System.out.println(tokens[PATH]);
-                String[] paths = tokens[PATH].split("/");
-
-                String[] queryStrings = paths[BEHAVIOR].split("&");
-                for(String query: queryStrings){
-                    System.out.println("query ; " + query);
-                }
-//                body = "Success".getBytes();
-            }
 
             if(line == null) return;
 
-            while(!line.equals("")){
-                line = br.readLine();
-                System.out.println("line : " + line);
-            }
+            String[] tokens = line.split(" ");
 
-            DataOutputStream dos = new DataOutputStream(out);
-//            byte[] body = "Hello world".getBytes();
+            printRequestHandler(br, line);
 
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            if(serveIndexFile(tokens, out)) return;
+
+            analyzeQueryParameter(tokens[QUERY]);
+
+
+
+
+
+
+//            byte[] body = "".getBytes();
+//
+//            if(tokens[PATH].contains(".")){
+//                String prefix = "/Users/changsuyouk/java/src/webserver/webapp";
+//                body = readFile(prefix + tokens[PATH]);
+//            } else {
+//                System.out.println(tokens[PATH]);
+//                String[] paths = tokens[PATH].split("/");
+//                for(String path: paths){
+//                    System.out.println("path : " + path);
+//                }
+//                String[] queryStrings = paths[BEHAVIOR].split("&");
+//                queryStrings[COMMAND] = queryStrings[COMMAND].split("\\?")[QUERY];
+//                for(String query: queryStrings){
+//                    System.out.println("query : " + query);
+//                }
+////                body = "Success".getBytes();
+//            }
+
+
         } catch (IOException e){
             e.printStackTrace();
         }
+    }
+
+    private void analyzeQueryParameter(String parameter){
+        String[] paths = parameter.split("/");
+//        for (String path : paths){
+//            System.out.println("path : " + path);
+//        }
+
+        String[] tokens = paths[BEHAVIOR].split("\\?");
+        if (tokens[COMMAND].equals("create")){
+            String[] parameters = tokens[QUERY].split("&");
+
+            ArrayList<String> keys = new ArrayList<>();
+            ArrayList<String> values = new ArrayList<>();
+
+            for (String param : parameters){
+                String key = param.split("=")[KEY];
+                String value = param.split("=")[VALUE];
+                keys.add(key);
+                values.add(value);
+                System.out.println(key + " : " + value);
+            }
+        }
+    }
+
+    private boolean serveIndexFile(String[] tokens, OutputStream out){
+        if(tokens[PATH].equals("/") || tokens[PATH].equals("/index.html")){
+            System.out.println("first line : " + tokens[PATH]);
+            String indexFile = "/index.html";
+            String prefix = "/Users/changsuyouk/java/src/webserver/webapp";
+            body = readFile(prefix + indexFile);
+            makeResponse(out, body);
+            return true;
+        }
+        return false;
+    }
+
+    private void makeResponse(OutputStream out, byte[] body){
+        DataOutputStream dos = new DataOutputStream(out);
+        response200Header(dos, body.length);
+        responseBody(dos, body);
+    }
+
+    private void printRequestHandler(BufferedReader br, String line){
+        System.out.println("==== Request Handler ====");
+        while(!line.equals("")){
+            System.out.println("|  " + line);
+            try {
+                line = br.readLine();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+
+        }
+        System.out.println("=========================");
+        System.out.println("");
     }
 
     private byte[] readFile(String requestPath){
@@ -113,3 +182,19 @@ public class RequestHandler extends Thread {
         }
     }
 }
+
+
+// REQUEST HEADER
+// GET / HTTP/1.1
+// Host: localhost:8080
+// Connection: keep-alive
+// Cache-Control: max-age=0
+// Upgrade-Insecure-Requests: 1
+// User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36
+// Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+// DNT: 1
+// Accept-Encoding: gzip, deflate, sdch
+// Accept-Language: en-US,en;q=0.8,ko;q=0.6
+// Cookie: gsScrollPos=0
+
+
--- src/webserver/model/User.java
+++ src/webserver/model/User.java
@@ -23,5 +23,4 @@ public class User {
                 " " + this.name +
                 " " + this.email;
     }
-
 }
